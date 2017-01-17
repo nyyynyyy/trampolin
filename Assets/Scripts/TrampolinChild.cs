1e355d809151b9eb94ab5ea5137aba5a823bb5f4
nyyynyyy <nyyynyyy@naver.com>
2017-01-17 15:38:11 +0900

    Trampolin

---
+++ Assets/Scripts/TrampolinChild.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolinChild : MonoBehaviour {
+    
+    private float _jumpForce;
+
+    public float jumpForce
+    {
+        set
+        {
+            _jumpForce = value;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        Rigidbody rigid = other.gameObject.GetComponent<Rigidbody>();
+        Vector3 jumpVecter = new Vector3(0, _jumpForce);
+
+        Debug.Log("JUMP");
+
+        rigid.velocity = new Vector3(rigid.velocity.x , 0, rigid.velocity.z); // 중력 가속도 초기화
+        rigid.AddForce(jumpVecter);
+    }
+}
