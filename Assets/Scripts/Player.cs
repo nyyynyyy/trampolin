6cf1044e0f6b7fe0f6c90af8424974d9cf6f0f50
jinwoo1234 <skdiwlsdn5@naver.com>
2017-01-17 15:37:12 +0900

    movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+    public float _movespeed = 10f;
+    public float _turnspeed = 180f;
+    public float _movepoint;
+
+    private float _move;
+    private float _rotate;
+    private Rigidbody _rigidbody;
+
 	void Start () {
-		
+        _rigidbody = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        _move = Input.GetAxis("Vertical");
+        _rotate = Input.GetAxis("Horizontal");
 	}
+
+    private void FixedUpdate() {
+        Moving();
+        Turning();
+    }
+
+    private void Moving() {
+        Vector3 movement = transform.forward * _move * Time.deltaTime * _movespeed;
+
+        _rigidbody.MovePosition(_rigidbody.position + movement);
+        
+    }
+
+    private void Turning() {
+        float turn = _rotate * Time.deltaTime * _turnspeed;
+
+        Quaternion _turnrotate = Quaternion.Euler(0f, turn, 0f);
+
+        _rigidbody.MoveRotation(_rigidbody.rotation * _turnrotate);
+    }
 }
