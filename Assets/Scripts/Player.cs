f4f9797040668be2129a89985e9e71f7701cad20
jinwoo1234 <skdiwlsdn5@naver.com>
2017-01-18 12:14:25 +0900

    jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour {
 
     public float _movespeed = 10f;
     public float _turnspeed = 180f;
-    public float _movepoint;
+    public float _jumphigh = 500f;
 
     private float _move;
     private float _rotate;
     private Rigidbody _rigidbody;
+    private bool _jump;
+    private bool _jumpon;
 
 	void Start () {
         _rigidbody = GetComponent<Rigidbody>();
@@ -19,18 +21,36 @@ public class Player : MonoBehaviour {
 	void Update () {
         _move = Input.GetAxis("Vertical");
         _rotate = Input.GetAxis("Horizontal");
+        _jump = Input.GetKeyDown(KeyCode.Space);
 	}
 
     private void FixedUpdate() {
-        Moving();
+        
         Turning();
+        if (_jumpon == false) {
+            Moving();
+            if (_jump == true)
+            {
+                Jumping();
+            }
+        }
+        else {
+            JumpMoving();
+        }
     }
 
     private void Moving() {
         Vector3 movement = transform.forward * _move * Time.deltaTime * _movespeed;
 
         _rigidbody.MovePosition(_rigidbody.position + movement);
-        
+    }
+
+    private void JumpMoving() {
+        if(_move < 0) _move = 0;
+
+        Vector3 movement = transform.forward * _move * Time.deltaTime * _movespeed;
+
+        _rigidbody.MovePosition(_rigidbody.position + movement);
     }
 
     private void Turning() {
@@ -39,5 +59,28 @@ public class Player : MonoBehaviour {
         Quaternion _turnrotate = Quaternion.Euler(0f, turn, 0f);
 
         _rigidbody.MoveRotation(_rigidbody.rotation * _turnrotate);
+    }
+    
+    private void Jumping() {
+        _rigidbody.AddForce(new Vector3(0, _jumphigh, 0));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // 착지
+        string tag = other.collider.tag;
+
+        if(tag == "Ground")
+            _jumpon = false;
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        // 점프
+        string tag = other.collider.tag;
+
+        if(tag == "Ground")
+            _jumpon = true;
     }
 }
