9ed1bd35c4f4b18d698c1375a96ec311150a5c86
nyyynyyy <nyyynyyy@naver.com>
2017-01-19 15:27:32 +0900

    Drop Candy & Call Baby

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour {
+
+    public Baby _baby;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag == "Ground")
+        {
+            _baby.DropCandy(transform.position);
+            Debug.Log("CALL BABY : " + transform.position);
+        }
+
+        if(other.collider.tag == "Baby")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+}
