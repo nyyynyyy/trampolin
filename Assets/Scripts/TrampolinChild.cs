26ec4055e64c5cf97333a26d22858d25007da34e
jinwoo1234 <skdiwlsdn5@naver.com>
2017-01-24 15:20:29 +0900

    audio

--- Assets/Scripts/TrampolinChild.cs
+++ Assets/Scripts/TrampolinChild.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrampolinChild : MonoBehaviour {
-    
+
+    public AudioClip _jumpSoundClip;
+    public AudioSource _jumpSound;
+
     private float _jumpForce;
 
     public float jumpForce
@@ -16,14 +19,19 @@ public class TrampolinChild : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        _jumpSound.clip = _jumpSoundClip;
+        _jumpSound.loop = false;
+        _jumpSound.pitch = 0.65f;
+        _jumpSound.Play();
         Debug.Log("JUMP");
+        
         if (other.tag != "Player" && other.tag != "Baby") return;
 
         Rigidbody rigid = other.gameObject.GetComponent<Rigidbody>();
         Vector3 jumpVecter = new Vector3(0, _jumpForce);
-
+        
         Debug.Log("JUMP");
-
+        
         rigid.velocity = new Vector3(rigid.velocity.x , 0, rigid.velocity.z); // 중력 가속도 초기화
         rigid.AddForce(jumpVecter);
     }
